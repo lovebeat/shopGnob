bb79f046cb11a30ee4cb79422a7c5b925af04713
XuanDuong <09xuanduong@gmail.com>
2017-09-22 22:31:36 +0700

    loading static resources: successfull. haizz.... :3

---
+++ shopGnob/src/main/java/me/you/shopGnob/controller/PageController.java
@@ -0,0 +1,38 @@
+package me.you.shopGnob.controller;
+
+import javax.jws.WebParam.Mode;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to spring Web MVC");
+		return mv;
+	}
+
+	@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam(value = "greeting", required = false) String greeting) {
+		if (greeting == null) {
+			greeting = "gnous";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+
+	@RequestMapping(value = "/testt/{greeting}")
+	public ModelAndView testt(@PathVariable(value = "greeting") String greeting) {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+}
